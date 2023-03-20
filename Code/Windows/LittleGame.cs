f1d1e277bdf5cf7157391239dbb717f9fe326013
michelenatale <104991886+michelenatale@users.noreply.github.com>
2023-03-20 18:21:38 +0100

    Add files via upload

---
+++ Code/Windows/LittleGame.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace michele.natale.Games;
+
+public partial class LittleGame
+{
+  public void Start(FrmMain? frmmain)
+  {
+    Start();
+    using var frm = frmmain;
+    frm!.LittleGameHandler += App_LittleGame!;
+    frm!.ShowDialog();
+  }
+}
---
+++ Code/Windows/Random/RandomHolder.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace michele.natale.Randoms;
+
+public class RandomHoder
+{
+  public static readonly Random Rand = new();
+
+  public static void WarmUp()
+  {
+    var cnt = Rand.Next() % 1000 + 1234;
+    for (var i = 0; i < cnt; i++) Rand.Next();
+  }
+}
